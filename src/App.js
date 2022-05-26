5956b36dd1a08658c30d615e66ba984c7594524f
nick johnson <njohnson144@hotmail.com>
2022-05-26 14:44:14 -0400

    display products on home page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,52 @@ import LogInPage from './components/login/LogInPage';
 import SignUpPage from './components/login/SignUpPage';
 // import UserPage from './components/UserPage';
 import { createContext, useEffect, useState } from 'react';
+import ProductCard from '../src/productCard/ProductCard';
+import { useApi } from './services/axios.service';
+import { useLocalStorage } from './services/localStorage.service';
 
-
-
-const axiosService = require('./services/axios.service');
-const localStorageService = require('./services/localStorage.service');
 
 export const Context = createContext();
 
-
 function App() {
+
+  const [watches, setWatches] = useState([])
+  const ls = useLocalStorage();
+  let user = ls.getUser();
+
+
+  const http = useApi();
+
+  function getWatches() {
+    http.getAllWatches()
+      .then((response) => {
+        console.log(response)
+        setWatches(response.data.watches);
+      })
+      .catch(() => {
+        console.log("error getting all")
+      })
+  }
+
+  useEffect(() => {
+    getWatches();
+  }, []);
+
+  // state value for your data
+
+  // function to get data via http request
+
+  // use effect => get data
+
   return (
     <div className="App">
-      App
+
+      <div className='products-container'>
+
+      {watches.map(watch => <ProductCard key={watch.id} {...watch} />)}
+      </div>
+
+      {/* array.map data => cards */}
     </div>
   );
 }
