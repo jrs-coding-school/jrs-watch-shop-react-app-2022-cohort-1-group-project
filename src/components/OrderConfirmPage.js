cf78213f786b568b4e4d489b02487257133d30f9
Cjganderson1 <97189087+Cjganderson1@users.noreply.github.com>
2022-06-01 14:23:35 -0400

    orderConfirmPage component comnplete changes

--- src/components/OrderConfirmPage.js
+++ src/components/OrderConfirmPage.js
@@ -1,17 +1,104 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './OrderConfirm.css'
+import { useApi } from '../services/axios.service';
+import { useParams } from 'react-router-dom';
 
 export default function OrderConfirmPage() {
+
+    let { transactionId } = useParams()
+    // let transactionId = '9ea27064-ddcd-11ec-902c-ef6a451bb744'
+
+    const [order, setOrder] = useState({});
+    const [items, setItems] = useState([]);
+    // const ls = useLocalStorage();
+    //trans id to test in orderconfirm url: 9ea27064-ddcd-11ec-902c-ef6a451bb744
+
+    const http = useApi();
+
+    function getItemsPurchasedByTransactionId() {
+        http.getItemsPurchasedByTransactionId(transactionId)
+            .then((response) => {
+                console.log("you're in the order confirm page server response ")
+                console.log(response)
+                setItems(response.data.items);
+                // console.log(response.data.response[0]);
+            })
+            .catch(() => {
+                console.log("error getting all")
+            })
+    }
+
+    function getTransactionData() {
+        http.getTransactionById(transactionId)
+            .then((response) => {
+                console.log(response)
+                setOrder(response.data.transaction);
+            })
+            .catch(() => {
+                console.log("error getting transaction data")
+            })
+    }
+
+    useEffect(() => {
+        console.log("im in the useEffect function")
+        getTransactionData();
+        getItemsPurchasedByTransactionId();
+    }, []);
+
     return (
         <div className="order-root">
-            <h1>
-                ** Order is confirmed **
-            </h1>
-            <div className="confirm-header">Thank you, your purchase was succesful. </div>
-            <div> image name, brand and color goes here</div>
 
-            <div> Total Cost: $ { }</div>
-            <div className="item-root"></div>
+            <div className='order-page-content'>
+                <div className="order-status-root">
+                    <div className="order-status1">
+                        <b>
+                            {transactionId == order.id
+                                ? `Thank you for shopping with us ${console.log(order.id)}`
+                                : `Order '${transactionId}' Doesnt exist`}
+
+                        </b>
+
+                    </div>
+
+                    <div className="order-status2">
+
+                        {order.id
+                            ? <b>** Your Order was successful ** <br>
+                            </br>  Order Number: {order.id}</b>
+                            : <b>Your order was not succesful, please try again</b>}
+
+                    </div>
+                </div>
+
+                {items.map((item) => (
+                    <div className="order-detail-root " key={item.id}>
+                        <div className="order-display">
+                            <div className="product-image">
+                                <img className='image' src={item.image} />
+
+                            </div>
+
+                            <div className="order-data-root">
+                                {/* <div className="orderId-display">
+                                    {transactionId == order.id ? `Order Number: ${order.id}` : ''}
+                                </div> */}
+                                <div className="orderDate-display">
+                                    {transactionId == order.id && order.date ? `Date: ${order.date}` : ''}
+                                </div>
+                                {/* <div className="order-display-id"> Product ID: {item.id}</div> */}
+                                <div className="order-display-brand"> Brand:  {item.brand} </div>
+                                <div className="order-display-name"> Name: {item.name} </div>
+                                <div className="order-display-color"> Color: {item.color} </div>
+                                <div className="order-display-quantity"> Qty: {item.quantity} </div>
+                                <div className="order-display-total"> Total Price: ${item.total} </div>
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
     )
+
 }
